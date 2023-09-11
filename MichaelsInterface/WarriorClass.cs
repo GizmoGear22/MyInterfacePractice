8a9f61c3cab5c6bd160db6f59a9c10c9ad8262c5
Michael <mpsimmer@gmail.com>
2023-09-11 19:02:06 -0400

    Initial commit

---
+++ MichaelsInterface/WarriorClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MichaelsInterface
+{
+    internal class WarriorClass : charClasses, IPlayClasses
+    {
+        public WarriorClass(string name)
+        {
+            this.Name = name;
+            this.Level = 1;
+        }
+
+
+    }
+}
---
+++ MichaelsInterface/charClasses.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MichaelsInterface
+{
+    internal class charClasses : IPlayClasses
+    {
+        public int Health { get; set; }
+        public string Name { get; set; }
+        public int Level { get; set; }
+
+        public void IncreaseLevel()
+        {
+            Level += 1;
+        }
+    }
+}
